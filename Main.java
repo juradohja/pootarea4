fed6d5df9efd76d6857921493ea0d0821584a033
juradohja <keissi.nanna@hotmail.com>
2016-10-25 08:44:34 -0500

    Added class MiVentana

--- Main.java
+++ Main.java
@@ -2,17 +2,10 @@ import java.util.*;
 
 public class Main{
 	public static void main(String[] args) {
+		Usuario usuario = new Usuario("Nombre", "Correo", "Contrasena");
+		MiVentana window = new MiVentana(usuario);
 
-		Scanner scanner=new Scanner(System.in);
-		System.out.println("Nombre:");
-		String nombre=scanner.nextLine();
-		System.out.println("Correo electrónico:");
-		String correo=scanner.nextLine();
-		System.out.println("Contraseña:");
-		String contrasena=scanner.nextLine();
-		Usuario usuario = new Usuario(nombre, correo, contrasena);
-
-		for(int i=0;i<usuario.getCarrito().getArregloLength();i++){
+	/*	for(int i=0;i<usuario.getCarrito().getArregloLength();i++){
 			System.out.println("Descripción artículo "+(i+1)+":");
 			String descripcion=scanner.nextLine();
 			System.out.println("Precio artículo "+(i+1)+":");
@@ -20,7 +13,6 @@ public class Main{
 			Articulo articulo=new Articulo(descripcion, precio);
 			usuario.getCarrito().agregarArticulo(articulo, i);
 		}
-
-		usuario.getCarrito().finalizarCompra();
+		usuario.getCarrito().finalizarCompra(); */
 	}
 }
---
+++ MiVentana.java
@@ -0,0 +1,77 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MiVentana extends JFrame{
+	
+	private int width=400, height=400, cont=0;
+	private JLabel desc1, desc2, desc3, total;
+	private JTextField char1, char2, char3;
+	private JButton addArticulo;
+	private Usuario usuario;
+
+	public MiVentana(Usuario usuario){
+		setSize(width,height);
+		setTitle("Tarea 4");
+		setLayout(new FlowLayout());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		initComponents();
+		setVisible(true);
+		this.usuario=usuario;
+	}
+
+	public void initComponents(){
+		desc1 = new JLabel("Artículo:");
+		desc2 = new JLabel("Talla:");
+		desc3 = new JLabel("Precio:");
+		total = new JLabel();
+		char1 = new JTextField(20);
+		char1.addActionListener(new FieldListener());
+		char2 = new JTextField(20);
+		char2.addActionListener(new FieldListener());
+		char2.setText("0");
+		char3 = new JTextField(20);
+		char3.addActionListener(new FieldListener());
+		addArticulo = new JButton("Agregar producto");
+		addArticulo.addActionListener(new ButtonListener());
+		add(desc1);
+		add(char1);
+		add(desc2);
+		add(char2);
+		add(desc3);
+		add(char3);
+		add(addArticulo);
+		add(total);
+	}
+
+	public class FieldListener implements ActionListener{
+		public void actionPerformed(ActionEvent event){
+			
+		}
+	}
+
+	public class ButtonListener implements ActionListener{
+		public void actionPerformed(ActionEvent event){
+			if(cont<5){
+				String d = char1.getText();
+				int t = Integer.parseInt(char2.getText());
+				double p = Double.parseDouble(char3.getText());
+				if(t==0){
+					Articulo articulo = new Articulo(d, p);
+					usuario.getCarrito().agregarArticulo(articulo, cont);
+				} else {
+					Articulo articulo = new Ropa(d, p, t);
+					usuario.getCarrito().agregarArticulo(articulo, cont);
+				}
+				cont++;
+			}
+			char1.setText("");
+			char2.setText("0");
+			char3.setText("");
+			if(cont==5){
+				total.setText("Precio final: $"+usuario.getCarrito().calcularPrecioFinal());
+			}
+		}
+	}
+
+}
